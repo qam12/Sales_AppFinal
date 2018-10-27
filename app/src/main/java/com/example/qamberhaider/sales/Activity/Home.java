372a7122f13f26ecf62a831d345d158c5fbfe222
qam12 <qamb565@gmail.com>
2018-10-27 12:00:06 +0500

    Regisration commit

--- app/src/main/java/com/example/qamberhaider/sales/Activity/Home.java
+++ app/src/main/java/com/example/qamberhaider/sales/Activity/Home.java
@@ -11,5 +11,7 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+//        Working in process
     }
 }
